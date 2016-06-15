4f1d93f68bb2a0a3136242a0c0419687201ef63f
artJava <artJava@192.168.56.1>
2016-06-16 01:19:34 +0800

    oragin 	-- artJava

---
+++ art-common/src/main/java/org/artJava/art_common/App.java
@@ -0,0 +1,13 @@
+package org.artJava.art_common;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ art-parent/src/main/java/org/artJava/parent/App.java
@@ -0,0 +1,13 @@
+package org.artJava.parent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
